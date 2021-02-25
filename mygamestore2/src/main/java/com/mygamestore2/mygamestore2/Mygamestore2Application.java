afd75d7e7dae19a429e3c1ff15251607e8e5182e
Milton-668 <miltonmjcp@gmail.com>
2021-02-25 15:03:54 -0300

    Criação do Projeto

---
+++ mygamestore2/src/main/java/com/mygamestore2/mygamestore2/Mygamestore2Application.java
@@ -0,0 +1,13 @@
+package com.mygamestore2.mygamestore2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mygamestore2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mygamestore2Application.class, args);
+	}
+
+}
---
+++ mygamestore2/src/test/java/com/mygamestore2/mygamestore2/Mygamestore2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mygamestore2.mygamestore2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mygamestore2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
